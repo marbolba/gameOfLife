ce28dc2ee1c3afd1977d667d0e8716028421748b
Marcin Bołba <marcin.bolba@wp.pl>
2019-03-13 14:29:23 +0100

    Add Fractions, fair game option
    If cell neighbours  is for example [2,2,0,0], cell stays with color

--- script/draw.js
+++ script/draw.js
@@ -21,36 +21,61 @@ initAliveTable = () => {
 	rowNumber = rowNumberLoc;
 	for(let i=0 ; i<rowNumber ;i++){
 		let colArr = [];
-		for(let i=0 ; i<columnNumber ;i++){
-			colArr.push(0);
+		for(let j=0 ; j<columnNumber ;j++){
+			if(i<rowNumber/2 && j<columnNumber/2 )			//fract 1 
+				colArr.push({alive:0,fraction:0});
+			if(i<rowNumber/2 && j>=columnNumber/2 )			//fract 2 
+				colArr.push({alive:0,fraction:1});
+			if(i>=rowNumber/2 && j<columnNumber/2 )			//fract 3 
+				colArr.push({alive:0,fraction:2});
+			if(i>=rowNumber/2 && j>=columnNumber/2 )		//fract 4 
+				colArr.push({alive:0,fraction:3});
 		}
 		aliveTable[i] = colArr;
 	}
 }
-drawSingleTriangle = (alive) => {
+drawSingleTriangle = (individual) => {
     var ctx = canvas.getContext('2d');
 
 	ctx.beginPath();
     ctx.moveTo(currPosX,currPosY);
     ctx.lineTo(currPosX+triangleSideLength,currPosY);
 	ctx.lineTo(currPosX+triangleSideLength/2,currPosY+triangleHeight);
-	if(alive===0){
-		ctx.fillStyle = "#e6e6e6";
+	if(individual.alive===1){
+		if(individual.fraction === 0){
+			//mozna porobic rozne kolorki dla atrakccyjnosci
+			ctx.fillStyle = "#00ff00";
+		}else if(individual.fraction === 1){
+			ctx.fillStyle = "#000066";
+		}else if(individual.fraction === 2){
+			ctx.fillStyle = "#ff6600";
+		}else if(individual.fraction === 3){
+			ctx.fillStyle = "#996633";
+		}
 	}else{
-		ctx.fillStyle = "#000000";
+		ctx.fillStyle = "#d9d9d9";
 	}
     ctx.fill();
 }
-drawReverseTriangle = (alive) => {
+drawReverseTriangle = (individual) => {
     var ctx = canvas.getContext('2d');
 	ctx.beginPath();
     ctx.moveTo(currPosX,currPosY);
     ctx.lineTo(currPosX+triangleSideLength,currPosY);
     ctx.lineTo(currPosX+triangleSideLength/2,currPosY-triangleHeight);
-	if(alive===0){
-		ctx.fillStyle = "#f2f2f2";
+	if(individual.alive===1){
+		if(individual.fraction === 0){
+			//mozna porobic rozne kolorki dla atrakccyjnosci
+			ctx.fillStyle = "#00ff00";
+		}else if(individual.fraction === 1){
+			ctx.fillStyle = "#000066";
+		}else if(individual.fraction === 2){
+			ctx.fillStyle = "#ff6600";
+		}else if(individual.fraction === 3){
+			ctx.fillStyle = "#996633";
+		}
 	}else{
-		ctx.fillStyle = "#0d0d0d";
+		ctx.fillStyle = "#bfbfbf";
 	}
 	ctx.fill();
 }
@@ -99,7 +124,7 @@ applyInputs = () => {
 		if(newDensity === 100){
 			aliveTable.forEach((row,index) => {
 				row.forEach((element,elementIndex) => {
-					aliveTable[index][elementIndex] = 1;
+					aliveTable[index][elementIndex].alive = 1;
 				})
 			})
 		}else{
@@ -114,8 +139,8 @@ applyInputs = () => {
 aliveRandomCell = () => {
 	let row = Math.floor((Math.random() * rowNumber));
 	let col = Math.floor((Math.random() * columnNumber))
-	if(aliveTable[row][col]===0)
-		aliveTable[row][col] = 1;
+	if(aliveTable[row][col].alive===0)
+		aliveTable[row][col].alive = 1;
 	else
 		aliveRandomCell();
 }
@@ -131,28 +156,40 @@ nextStep = () => {
 	let tmpAliveArr = [];
 	for(let i=0 ; i<rowNumber ;i++){
 		let colArr = [];
-		for(let i=0 ; i<columnNumber ;i++){
-			colArr.push(0);
-		}
+		for(let j=0 ; j<columnNumber ;j++){
+			if(i<rowNumber/2 && j<columnNumber/2 )			//fract 1 
+				colArr.push({alive:0,fraction:0});
+			if(i<rowNumber/2 && j>=columnNumber/2 )			//fract 2 
+				colArr.push({alive:0,fraction:1});
+			if(i>=rowNumber/2 && j<columnNumber/2 )			//fract 3 
+				colArr.push({alive:0,fraction:2});
+			if(i>=rowNumber/2 && j>=columnNumber/2 )		//fract 4 
+				colArr.push({alive:0,fraction:3});
+			}
 		tmpAliveArr[i] = colArr;
 	}
-
+//ZASADY ZYCIA I SMIERCI
+	let counter = 0;
 	aliveTable.forEach((row,index) => {
-		row.forEach((element,elementIndex) => {
+		row.forEach((element,elementIndex) => {	
 			if(index %2==0){
 				//Normal
-				let neighboursCount = getNormalNeighboursCount(index,elementIndex);
-				if(neighboursCount!==0){
-					if(neighboursCount>1&&neighboursCount<3)
-						tmpAliveArr[index][elementIndex] = 1;
+				let neighboursInfo = getNormalNeighboursCount(index,elementIndex);
+				if(neighboursInfo.count!==0){
+					if(neighboursInfo.count>1&&neighboursInfo.count<3)
+						tmpAliveArr[index][elementIndex].alive = 1;
+						tmpAliveArr[index][elementIndex].fraction = (neighboursInfo.nextFractionNr !== -1 ? neighboursInfo.nextFractionNr : element.fraction);
 				}
+				++counter;
 			}else{
 				//Reverse
-				let neighboursCount = getReverseNeighboursCount(index,elementIndex);
-				if(neighboursCount!==0){
-					if(neighboursCount>1&&neighboursCount<3)
-						tmpAliveArr[index][elementIndex] = 1;
+				let neighboursInfo = getReverseNeighboursCount(index,elementIndex);
+				if(neighboursInfo.count!==0){
+					if(neighboursInfo.count>1&&neighboursInfo.count<3)
+						tmpAliveArr[index][elementIndex].alive = 1;
+						tmpAliveArr[index][elementIndex].fraction = (neighboursInfo.nextFractionNr !== -1 ? neighboursInfo.nextFractionNr : element.fraction);
 				}
+				++counter;
 			}
 		})
 	})
@@ -160,46 +197,73 @@ nextStep = () => {
 	drawBoard();
 }
 getNormalNeighboursCount = (row,col) => {
-	let neighboursCount = 0;
+	let neighboursInfo = {count:0,fraction:[0,0,0,0],nextFractionNr:-1};
 	let rm1 = row-1 >= 0 			? row-1 : rowNumber-1;
 	let rp1 = row+1 < rowNumber 	? row+1 : 0;
 	let rp3 = row+3 < rowNumber		? row+3 : (row-rowNumber)+3;
 	let cm1 = col-1 >= 0 			? col-1 : columnNumber-1;
 	let cp1 = col+1 < columnNumber 	? col+1 : 0;
-	
-	if(aliveTable[rm1][col]===1)
-		++neighboursCount;
-	if(aliveTable[rm1][cm1]===1)
-		++neighboursCount;
-	if(aliveTable[rm1][cp1]===1)
-		++neighboursCount;
-	if(aliveTable[rp1][cm1]===1)
-		++neighboursCount;
-	if(aliveTable[rp1][cp1]===1)
-		++neighboursCount;
-	if(aliveTable[rp3][col]===1)
-		++neighboursCount;
-	return neighboursCount;
+	let currCellFraction = aliveTable[row][col].fraction;
+
+
+	//wypelnienie obiektu neigoburs info, tablica mowi o czlonkach konktretnej frakcji
+	conditionIf(rm1,col,neighboursInfo,currCellFraction);
+	conditionIf(rm1,cm1,neighboursInfo,currCellFraction);
+	conditionIf(rm1,cp1,neighboursInfo,currCellFraction);
+	conditionIf(rp1,cm1,neighboursInfo,currCellFraction);
+	conditionIf(rp1,cp1,neighboursInfo,currCellFraction);
+	conditionIf(rp3,col,neighboursInfo,currCellFraction);
+
+	let indexOfNextFraction = indexOfMax(neighboursInfo.fraction);
+		neighboursInfo.nextFractionNr = indexOfNextFraction;
+	return neighboursInfo;
 }
 getReverseNeighboursCount = (row,col) => {
-	let neighboursCount = 0;
+	let neighboursInfo = {count:0,fraction:[0,0,0,0],nextFractionNr:-1};
 	let rm1 = row-1 >= 0 			? row-1 : rowNumber-1;
 	let rp1 = row+1 < rowNumber 	? row+1 : 0;
 	let rm3 = row-3 >= 0			? row-3 : rowNumber-(3-row);
 	let cm1 = col-1 >= 0 			? col-1 : columnNumber-1;
 	let cp1 = col+1 < columnNumber 	? col+1 : 0;
-	
-	if(aliveTable[rm3][col]===1)
-		++neighboursCount;
-	if(aliveTable[rm1][cm1]===1)
-		++neighboursCount;
-	if(aliveTable[rm1][cp1]===1)
-		++neighboursCount;
-	if(aliveTable[rp1][col]===1)
-		++neighboursCount;
-	if(aliveTable[rp1][cm1]===1)
-		++neighboursCount;
-	if(aliveTable[rp1][cp1]===1)
-		++neighboursCount;
-	return neighboursCount;
+	let currCellFraction = aliveTable[row][col].fraction;
+
+	conditionIf(rp1,col,neighboursInfo,currCellFraction);
+	conditionIf(rp1,cm1,neighboursInfo,currCellFraction);
+	conditionIf(rp1,cp1,neighboursInfo,currCellFraction);
+	conditionIf(rm3,col,neighboursInfo,currCellFraction);
+	conditionIf(rm1,cm1,neighboursInfo,currCellFraction);
+	conditionIf(rm1,cp1,neighboursInfo,currCellFraction);
+
+	let indexOfNextFraction = indexOfMax(neighboursInfo.fraction);
+	if(indexOfNextFraction !== -1)
+		neighboursInfo.nextFractionNr = indexOfNextFraction;
+	return neighboursInfo;
+}
+conditionIf = (row,column,neighboursInfo,currCellFraction) => {
+	if(aliveTable[row][column].alive === 1){
+		++neighboursInfo.count;
+		if(aliveTable[row][column].fraction === currCellFraction){
+			++neighboursInfo.fraction[currCellFraction];
+		}else{
+			++neighboursInfo.fraction[aliveTable[row][column].fraction];
+		}
+	}
+}
+indexOfMax = (arr) => {
+    if (arr.length === 0) {
+        return -1;
+	}
+    var max = 0;
+    var maxIndex = -1;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] >= max) {
+			if(arr[i] === max){
+				maxIndex = -1;
+			}else{
+				maxIndex = i;
+				max = arr[i];
+			}
+        }
+    }
+    return maxIndex;
 }
